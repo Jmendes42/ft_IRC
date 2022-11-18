9c7f1cf856a7561fc90416032e14d9ccbb040a4c
joao-mendes723 <joao.brandao.mendes@gmail.com>
2022-11-18 21:11:29 +0000

    update00

--- ClientHandler.hpp
+++ ClientHandler.hpp
@@ -28,6 +28,9 @@ void    ClientHandler::editClient(std::vector<std::string> &info, int control)
         _clients.back()->setUser(info[1]);
         _clients.back()->setHost(info[2]);
         _clients.back()->setReal(info[4]);
+        MSG("Client Created");
+        std::cout << "Nick-- " << _clients[0]->getNick() << " --User-- " << _clients[0]->getUser()
+            << " --Host-- " << _clients[0]->getHost() << " --Real-- " << _clients[0]->getReal() << std::endl;
     }
 }
 
@@ -42,25 +45,30 @@ void    ClientHandler::addClient(std::string const &msg, std::string const &pass
         start = pos + 1;
     }
     pos = msg.length();
-    info.push_back(msg.substr(start, msg.find(' ') - 1));
+    info.push_back(msg.substr(start, pos - start - 2)); //msg.find(' ') - 1)
 
     MSG(pos);
+    MSG(start);
 
     if (!info[0].compare("PASS")) {
         if (!info[1].compare(pass)) {
-            MSG("Client Created");
             _clients.push_back(new Client());
-        }
+            MSG(info[1]);
+    }
         else {
             MSG(info[1]);
-            MSG(pass);
+            MSG("Error: Wrong password!");
             exit(0);
         }
     }
-    else if (!info[0].compare("NICK"))
+    else if (!info[0].compare("NICK")) {
+        MSG(info[1]);
         editClient(info, 0);
-    else if (!info[0].compare("USER"))
+    }
+    else if (!info[0].compare("USER")) {
+        MSG(info[1]);
         editClient(info, 1);
+    }
 }
 
 // REMOVE BY NICK ! CHECK THE COMMAND TO REMOVE USER TO CHECK WHAT IS NEEDED
