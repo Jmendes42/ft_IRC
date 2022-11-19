f9408c897985b0477cb16b83caf11aa8023612a7
isousa <isousa@gmail.com>
2022-11-19 07:38:20 +0000

    Channel codes received on weechat

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -41,7 +41,10 @@ void    Server::_interpreter(std::string const &msg, int const &clientSocket) {
 		send(clientSocket, "001 jmendes\n", 13, 0);
 	}
 	if (!cmd.compare("JOIN")) {
-		send(clientSocket, "332 jmendes\n", 13, 0);
+		// send(clientSocket, "331 #tardis", 12, 0);
+		send(clientSocket, "isousa!isousa@0 JOIN #tardis\n332 isousa #tardis :wooo\n353 isousa #tardis :@isousa\n366 isousa #tardis :End of NAMES list\n", 125, 0);
+		//send(clientSocket, "353 isousa #tardis :@isousa\n", 29, 0);
+		//send(clientSocket, "366 isousa #tardis :End of NAMES list\n", 39, 0);
 		_channelHandler.addChannel(msg, new Client());
 	}
 	if (!cmd.compare("PRIVMSG"))
